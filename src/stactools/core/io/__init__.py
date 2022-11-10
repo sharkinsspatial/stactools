d368e830fa31b1adf501f4a7e66b53264be65dd1
sharkinsspatial <sharkinsgis@gmail.com>
2022-11-10 11:53:10 -0600

    Demonstration of kwargs passing with incorrect typing.

--- src/stactools/core/io/__init__.py
+++ src/stactools/core/io/__init__.py
@@ -1,6 +1,7 @@
 """Input/output utility functions and definitions."""
-
-from typing import Callable, Optional
+# mypy: ignore-errors
+import os
+from typing import Any, Callable, Optional
 
 import fsspec
 from pystac.stac_io import DefaultStacIO, StacIO
@@ -14,7 +15,9 @@ Used for reading, e.g. appending an Azure SAS Token or translating to a signed U
 """
 
 
-def read_text(href: str, read_href_modifier: Optional[ReadHrefModifier] = None) -> str:
+def read_text(
+    href: str, read_href_modifier: Optional[ReadHrefModifier] = None, **kwargs
+) -> str:
     """Reads a string from an href.
 
     If ``read_href_modifier`` is provided, then ``href`` will be passed through
@@ -26,14 +29,27 @@ def read_text(href: str, read_href_modifier: Optional[ReadHrefModifier] = None) 
         read_href_modifier (ReadHrefModifier, optional):
             A function to modify
             the provided href. Defaults to None.
+        **kwargs : Arbitrary keyword arguments that may be utilized by the concrete
+                implementation.
 
     Returns:
         str: The text as read from the href.
     """
     if read_href_modifier is None:
-        return StacIO.default().read_text(href)
+        return StacIO.default().read_text_from_href(href, **kwargs)
     else:
-        return StacIO.default().read_text(read_href_modifier(href))
+        return StacIO.default().read_text_from_href(read_href_modifier(href))
+
+
+def write_text(href: str, txt: str, **kwargs: Any) -> None:
+    """Writes text to an href.
+    Args:
+        href (str): The href to write to.
+        txt (str): The text to write.
+        **kwargs : Arbitrary keyword arguments that may be utilized by the concrete
+                implementation.
+    """
+    return StacIO.default().write_text_from_href(href, txt, **kwargs)
 
 
 class FsspecStacIO(DefaultStacIO):
@@ -41,17 +57,19 @@ class FsspecStacIO(DefaultStacIO):
     <https://filesystem-spec.readthedocs.io/en/latest/>`_ for reads and writes.
     """
 
-    def read_text_from_href(self, href: str) -> str:
+    def read_text_from_href(self, href: str, **kwargs: Any) -> str:
         """Reads a file as a utf-8 string using `fsspec
         <https://filesystem-spec.readthedocs.io/en/latest/>`_
 
         Args:
             href (str): The href to read.
-
+            **kwargs : Arbitrary keyword arguments that may be utilized by the concrete
+                implementation.
         Returns:
             str: The read text, decoded as utf-8 if necessary.
         """
-        with fsspec.open(href, "r") as f:
+        href = os.fspath(href)
+        with fsspec.open(href, "r", **kwargs) as f:
             s = f.read()
             if isinstance(s, str):
                 return s
@@ -60,22 +78,25 @@ class FsspecStacIO(DefaultStacIO):
             else:
                 raise ValueError(f"Unable to decode data loaded from HREF: {href}")
 
-    def write_text_from_href(self, href: str, txt: str) -> None:
+    def write_text_from_href(self, href: str, txt: str, **kwargs) -> None:
         utils.deprecate(
             "FsspecStacIO.write_text_from_href",
             "FsspecStacIO.write_text_to_href",
             "v0.5.0",
         )
-        return self.write_text_to_href(href, txt)
+        return self.write_text_to_href(href, txt, **kwargs)
 
-    def write_text_to_href(self, href: str, txt: str) -> None:
+    def write_text_to_href(self, href: str, txt: str, **kwargs: Any) -> None:
         """Writes text to an href using fsspec.
 
         Args:
             href (str): The href to write to.
             txt (str): The text to write.
+            **kwargs : Arbitrary keyword arguments that may be utilized by the concrete
+                implementation.
         """
-        with fsspec.open(href, "w") as destination:
+        href = os.fspath(href)
+        with fsspec.open(href, "w", **kwargs) as destination:
             destination.write(txt)
 
 
